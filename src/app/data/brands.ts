7326622ab5fe4ea902c753698f24622ce83d5206
Zakariyya <zakariyya.z@antetechnologies.com>
2026-07-02 22:41:59 +0400

    faridpt

--- src/app/data/brands.ts
+++ src/app/data/brands.ts
@@ -15,7 +15,7 @@ export const brands: Brand[] = [
   {
     id: "bwin",
     name: "Bwin",
-    logo: "/brands/bwin_dark.png",
+    logo: "/bwin_dark.png",
     rating: 9.9,
     bonus: "100% ATÉ €200 DE BÓNUS CASINO",
     url: "https://www.bwin.pt/?utm_source=casinotoppt&utm_medium=referral&clickid=",
